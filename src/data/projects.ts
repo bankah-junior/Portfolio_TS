6a6b4493d8afebc8faac34ef4d6ff7179dc13d93
Bankah Anthony Bekoe <154749514+bankah@users.noreply.github.com>
2024-10-23 23:18:26 +0000

    Added Trades to the Finance Section

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,40 +1,40 @@
+import MindMeld from '/public/assets/Projects/MindMeld.png';
+import MyRecipe from '/public/assets/Projects/MyRecipe.png';
+import ConnectToBuyers from '/public/assets/Projects/ConnectToBuyers.png';
+import BfxCalculator from '/public/assets/Projects/bfx-calculator.png';
+import GP from '/public/assets/Projects/GP.png';
+import Techie from '/public/assets/Projects/Techie.png';
+
+import Trade01 from "/public/assets/Trades/Trade01.png";
+import Trade02 from "/public/assets/Trades/Trade02.png";
+import Trade03 from "/public/assets/Trades/Trade03.png";
+import Trade04 from "/public/assets/Trades/Trade04.png";
+import Trade05 from "/public/assets/Trades/Trade05.png";
+import Trade06 from "/public/assets/Trades/Trade06.png";
+import Trade07 from "/public/assets/Trades/Trade07.png";
+import Trade08 from "/public/assets/Trades/Trade08.png";
+import Trade09 from "/public/assets/Trades/Trade09.png";
+import Trade10 from "/public/assets/Trades/Trade10.png";
+import Trade11 from "/public/assets/Trades/Trade11.png";
+import Trade12 from "/public/assets/Trades/Trade12.png";
+import Trade13 from "/public/assets/Trades/Trade13.png";
+import Trade14 from "/public/assets/Trades/Trade14.png";
+
 export const techProjects = [
-    /*{
-      title: 'AI Trading Bot',
-      description: 'Automated trading system using machine learning algorithms for market prediction',
-      technologies: ['Python', 'TensorFlow', 'APIs'],
-      githubUrl: 'https://github.com/bankah-junior/ai-trading-bot',
-      imageUrl: 'https://images.unsplash.com/photo-1642790106117-e829e14a795f?q=80&w=2070'
-    },
-    {
-      title: 'Portfolio Analytics Dashboard',
-      description: 'Real-time portfolio tracking and analysis platform',
-      technologies: ['React', 'Node.js', 'D3.js'],
-      githubUrl: 'https://github.com/bankah-junior/portfolio-analytics',
-      liveUrl: 'https://portfolio-analytics.demo.com',
-      imageUrl: 'https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2070'
-    },
-    {
-      title: 'Cloud Infrastructure Manager',
-      description: 'Automated cloud resource management and optimization tool',
-      technologies: ['AWS', 'Terraform', 'Go'],
-      githubUrl: 'https://github.com/bankah-junior/cloud-manager',
-      imageUrl: 'https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2072'
-    },*/
     {
       title: 'MindMeld',
       description: "This is an AI based website where users can read about AI. every article has its details when clicked. Explore the power of Artificial Intelligence and get to know the most asked questions about AI.",
       technologies: ['React', 'TailwindCSS'],
       githubUrl: 'https://github.com/bankah-junior/-MindMeld-',
       liveUrl: 'https://mindmeld.netlify.app',
-      imageUrl: '/assets/Projects/MindMeld.PNG',
+      imageUrl: MindMeld,
     },
     {
       title: 'My Recipe',
       description:"With this website, you get to perform the following functionalities: 'Add recipe', 'Delete recipe', 'Search recipe', 'Sort Recipe'. For the repository, visit my Github account.",
       technologies: ['Node.js', 'MongoDB', 'Express.js'],
       githubUrl: 'https://github.com/bankah-junior/My-Recipe',
-      imageUrl: '/assets/Projects/MyRecipe.PNG',
+      imageUrl: MyRecipe,
     },
     {
       title: 'Connect To Buyers',
@@ -42,7 +42,7 @@ export const techProjects = [
       technologies: ['HTML', 'CSS', 'JavaScript'],
       githubUrl: 'https://github.com/bankah-junior/Frontend-Part-2',
       liveUrl: 'https://ctbs.netlify.app',
-      imageUrl: '/assets/Projects/ConnectToBuyers.PNG',
+      imageUrl: ConnectToBuyers,
     },
     {
       title: 'BFX_Calculator',
@@ -50,7 +50,7 @@ export const techProjects = [
       technologies: ['React', 'TailwindCSS'],
       githubUrl: 'https://github.com/bankah-junior/BFX_Calculator.git',
       liveUrl: 'https://bfx-calculator.netlify.app/',
-      imageUrl: '/assets/Projects/bfx-calculator.png',
+      imageUrl: BfxCalculator,
     },
     {
       title: 'GP',
@@ -58,7 +58,7 @@ export const techProjects = [
       technologies: ['React', 'TailwindCSS'],
       githubUrl: 'https://github.com/bankah-junior/ZapTek-Internship/tree/3.GP',
       liveUrl: 'https://digitalmarketers.netlify.app/',
-      imageUrl: '/assets/Projects/GP.png',
+      imageUrl: GP,
     },
     {
       title: 'Techie',
@@ -66,66 +66,44 @@ export const techProjects = [
       technologies: ['Next.js', 'TailwindCSS'],
       githubUrl: 'https://github.com/bankah-junior/tech-site',
       liveUrl: 'https://techie-site.netlify.app/',
-      imageUrl: '/assets/Projects/Techie.png',
+      imageUrl: Techie,
     }
   ];
   
   export const financeProjects = [
-    /*{
-      title: 'Algorithmic Trading Strategy',
-      description: 'Mean reversion strategy with statistical arbitrage',
-      technologies: ['Python', 'Pandas', 'Backtrader'],
-      githubUrl: 'https://github.com/bankah-junior/algo-trading',
-      imageUrl: 'https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?q=80&w=2070'
-    },
-    {
-      title: 'Risk Management System',
-      description: 'Portfolio risk analysis and optimization platform',
-      technologies: ['R', 'Shiny', 'ggplot2'],
-      githubUrl: 'https://github.com/bankah-junior/risk-manager',
-      liveUrl: 'https://risk-manager.demo.com',
-      imageUrl: 'https://images.unsplash.com/photo-1590283603385-17ffb3a7f29f?q=80&w=2070'
-    },
-    {
-      title: 'Investment Research Platform',
-      description: 'Fundamental analysis and market research tool',
-      technologies: ['Vue.js', 'Flask', 'MongoDB'],
-      githubUrl: 'https://github.com/bankah-junior/investment-research',
-      imageUrl: 'https://images.unsplash.com/photo-1590283603385-17ffb3a7f29f?q=80&w=2070'
-    },*/ 
     {
       title: 'Strategy Statistics',
       description: 'Statistics of my strategy',
       technologies: ['Technical', 'Sentimental', 'Fundamental'],
-      imageUrl: '/assets/Projects/Strategy.jpeg',
+      imageUrl: '/public/assets/Projects/Strategy.jpeg',
     },
     {
       title: 'Assets',
       description: 'Percentage of trades in each different asset and by asset family (forex, commodities, indices, stocks...), as well as the accumulated return in each one of them.',
       technologies: ['Technical', 'Sentimental', 'Fundamental'],
-      imageUrl: '/assets/Projects/Assets.jpeg',
+      imageUrl: '/public/assets/Projects/Assets.jpeg',
     },
     {
       title: 'Trading Session',
       description: 'Distribution of trades between Asian (00:00-09:00 GMT), European (09:00-16:00 GMT) and American (16:00-00:00 GMT) sessions, as well as the accumulated return in each one of them.',
       technologies: ['Technical', 'Sentimental', 'Fundamental'],
-      imageUrl: '/assets/Projects/TradingSession.jpeg',
+      imageUrl: '/public/assets/Projects/TradingSession.jpeg',
     },
   ];
 
   export const TradeHistoryImages = [
-    "/assets/Trades/Trade01.png",
-    "/assets/Trades/Trade02.png",
-    "/assets/Trades/Trade03.png",
-    "/assets/Trades/Trade04.png",
-    "/assets/Trades/Trade05.png",
-    "/assets/Trades/Trade06.png",
-    "/assets/Trades/Trade07.png",
-    "/assets/Trades/Trade08.png",
-    "/assets/Trades/Trade09.png",
-    "/assets/Trades/Trade10.png",
-    "/assets/Trades/Trade11.png",
-    "/assets/Trades/Trade12.png",
-    "/assets/Trades/Trade13.png",
-    "/assets/Trades/Trade14.png",
+    Trade01,
+    Trade02,
+    Trade03,
+    Trade04,
+    Trade05,
+    Trade06,
+    Trade07,
+    Trade08,
+    Trade09,
+    Trade10,
+    Trade11,
+    Trade12,
+    Trade13,
+    Trade14
   ];
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Include PNG as asset files to be handled by Vite
+  assetsInclude: ['**/*.png', '**/*.PNG'],
 });
